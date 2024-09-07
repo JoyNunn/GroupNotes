17ed85658c3e2ddf52d65cfe85189c571b874515
Joy Nunn <joynunn77@gmail.com>
2024-09-07 19:55:35 -0400

    Daily notes update via script

--- CopyLatestNotes.sh
+++ CopyLatestNotes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+rm **AWSR*
 gdown --format html 1AFKa6DA1g61Izm_Z9G2jOSPjlxMydTSr05jzgPFUax4
 mv **AWS* AWSReStartLectureNotes.html
 rm '**AWS Re_*'
 git add **
 git commit -m "Daily notes update via script"
 git push -u origin master
-rm **AWSR*
