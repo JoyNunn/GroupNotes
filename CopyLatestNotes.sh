fb90258268d9c135cfa5f37a2331fbce834042ff
Joy Nunn <joynunn77@gmail.com>
2024-09-07 20:34:12 -0400

    Daily notes update via script

--- CopyLatestNotes.sh
+++ CopyLatestNotes.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+#Remove the previous day's HTML files.
 rm **AWSR*
+#Download the latest version of the Group Notes from Google Docs in HTML format. The long string is the document id. The "format" flag is how we're downloading it in html instead of docx. I don't currently know of a way to automatically get the images, so this happens manually.
 gdown --format html 1AFKa6DA1g61Izm_Z9G2jOSPjlxMydTSr05jzgPFUax4
+#Rename the downloaded file to something without spaces in it, just to make our lives easier.
 mv **AWS* AWSReStartLectureNotes.html
-rm '**AWS Re_*'
+#Add all of the files in the repository directory to the staging area to commit.
 git add **
+#Commit the changes to the local repository.
 git commit -m "Daily notes update via script"
+#Push the changes to the project page on GitHub.
 git push -u origin master
