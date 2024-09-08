aebf6fad7bbce280f25f311a8a8bcd8d6e978143
Joy Nunn <joynunn77@gmail.com>
2024-09-07 20:41:49 -0400

    Daily notes update via script

--- CopyLatestNotes.sh
+++ CopyLatestNotes.sh
@@ -3,7 +3,7 @@
 rm **AWSR*
 #Download the latest version of the Group Notes from Google Docs in HTML format. The long string is the document id. 
 #The "format" flag is how we're downloading it in html instead of docx. 
-#I don't currently know of a way to automatically get the images, so this happens manually.
+#The images in the document are linked to Google's CDN, so we don't have to worry about snagging them. It happens automatically.
 gdown --format html 1AFKa6DA1g61Izm_Z9G2jOSPjlxMydTSr05jzgPFUax4
 #Rename the downloaded file to something without spaces in it, just to make our lives easier.
 mv **AWS* AWSReStartLectureNotes.html
@@ -14,5 +14,4 @@ git commit -m "Daily notes update via script"
 #Push the changes to the project page on GitHub.
 git push -u origin master
 #From here, this script gets added to my cron jobs with the "crontab -e" command. 
-#It automatically updates the text at 5 PM everyday without any input from me. 
-#If I can find a way to download the images with gdown, this can all be automatic. 
+#It automatically updates the document at 5 PM everyday without any input from me. 
